0b93be451e8dd1b18fc7a70cd797ae5fce94b6ad
Nick Bartlett <nickbartlett19@gmail.com>
2023-10-05 10:37:29 -0400

    finish ex 22

--- fundamentals/22_filterunique/script.js
+++ fundamentals/22_filterunique/script.js
@@ -1,7 +1,7 @@
 function makeRandomArray(length) {
     let arr = []
     for (let i = 0; i < length; i++) {
-        arr.push(getRandInt(10));
+        arr.push(getRandInt(100));
     }
     return arr;
 }
@@ -11,11 +11,32 @@ function getRandInt(max) {
 }
 
 function makeArrUnique(array) {
-    
+    for (let i = 0; i < array.length; i++) {
+        if (searchForward(i, array) == true) {
+            array.splice(i, 1);
+            console.log("spliced!")
+        }
+    }
+    return array;
+}
+
+function searchForward(index, array) {
+    let fixedIndex = index;
+    for (let i = index + 1; i < array.length; i++) {
+        if (array[i] == array[fixedIndex]) {
+            return true
+        }
+    }
+    return false;
 }
 
 var myArray = makeRandomArray(10);
+// var myArray = [0,1,2,3,4,1,5,4];
 console.log(myArray);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(searchForward(i, myArray));
+// }
 
 var myArray = makeArrUnique(myArray);
 console.log(myArray);
